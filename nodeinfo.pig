62ac57d0f0ccb99530f8036a9afa05129a163531
Jeff Hodges <jeff@somethingsimilar.com>
2009-12-06 15:55:00 -0800

    Make LOG2 do all the type work for us.

--- nodeinfo.pig
+++ nodeinfo.pig
@@ -7,16 +7,16 @@ Y = FOREACH YGROUP GENERATE group, COUNT(WORDS) as wordpernodeperhourcount;
 XGROUP = GROUP Y BY (group.rawword, group.position, group.node);
 X = FOREACH XGROUP GENERATE group, SUM(Y.wordpernodeperhourcount) as wordpernodecount;
 
-XWITHLOG = FOREACH X GENERATE group, wordpernodecount, (wordpernodecount*nodeinfo.LOG2L(wordpernodecount)) as weirdwordpernode;
+XWITHLOG = FOREACH X GENERATE group, wordpernodecount, (wordpernodecount*nodeinfo.LOG2(wordpernodecount)) as weirdwordpernode;
 
 WORDGROUP = GROUP XWITHLOG BY (group.rawword, group.position);
 DANDE = FOREACH WORDGROUP GENERATE group, SUM(XWITHLOG.wordpernodecount) as dw, SUM(XWITHLOG.weirdwordpernode) as ew;
 
-ALLGW = FOREACH DANDE GENERATE group, (1.0 + (1.0 / nodeinfo.LOG2($numofnodes)) * (ew / dw - nodeinfo.LOG2L(dw))) as gw;
+ALLGW = FOREACH DANDE GENERATE group, (1.0 + (1.0 / nodeinfo.LOG2($numofnodes)) * (ew / dw - nodeinfo.LOG2(dw))) as gw;
 GW = FILTER ALLGW BY gw > 0.0;
 
 YANDG = JOIN GW BY (group.rawword, group.position), Y BY (group.rawword, group.position);
-PARTIAL = FOREACH YANDG GENERATE Y::group.rawword as rawword, Y::group.position as position, Y::group.node as node, Y::group.hour as hour, (GW::gw * nodeinfo.LOG2L(Y::wordpernodeperhourcount)) as partial;
+PARTIAL = FOREACH YANDG GENERATE Y::group.rawword as rawword, Y::group.position as position, Y::group.node as node, Y::group.hour as hour, (GW::gw * nodeinfo.LOG2(Y::wordpernodeperhourcount)) as partial;
 
 FULLGROUP = GROUP PARTIAL BY (node, hour);
 NODEINFO = FOREACH FULLGROUP GENERATE group.node, group.hour, SUM(PARTIAL.partial) as oddness;
--- src/main/java/LOG2.java
+++ src/main/java/LOG2.java
@@ -1,22 +1,66 @@
 package nodeinfo;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.pig.EvalFunc;
+import org.apache.pig.FuncSpec;
+import org.apache.pig.data.DataType;
 import org.apache.pig.data.Tuple;
+import org.apache.pig.impl.logicalLayer.FrontendException;
+import org.apache.pig.impl.logicalLayer.schema.Schema;
 import org.apache.pig.impl.util.WrappedIOException;
 
+// FIXME Only returns floored values of log base 2. This means it
+// won't really work for very small or very precise computations.
 public class LOG2 extends EvalFunc<Integer>
 {
-    // FIXME Only returns floored values of log base 2. This means it
-    // won't really work for very small or very precise computations.
+    private static Integer BITS = Integer.SIZE - 1;
+
     public Integer exec(Tuple input) throws IOException {
         if (input == null || input.size() == 0)
             return null;
         try{
-            Integer x = (Integer) input.get(0);
-            return (31 - Integer.numberOfLeadingZeros(x));
+            return log(input.get(0));
         }catch(Exception e){
             throw WrappedIOException.
                 wrap("In LOG2, caught exception processing input row ", e);
         }
+    }
+
+    public Integer log(Object somex) {
+        Integer x = (Integer) somex;
+        return (BITS - Integer.numberOfLeadingZeros(x));
+    }
+
+    static public class DoubleLog2 extends LOG2 {
+        private static Integer BITS = Long.SIZE - 1;
+        public Integer log(Object somex) {
+            Double x = (Double) somex;
+            return (BITS - Long.numberOfLeadingZeros(x.longValue()));
+        }
+    }
+
+    static public class LongLog2 extends LOG2 {
+        private static Integer BITS = Long.SIZE - 1;
+        public Integer log(Object somex) {
+            Long x = (Long) somex;
+            return (BITS - Long.numberOfLeadingZeros(x));
+        }
+    }
+
+    public List<FuncSpec> getArgToFuncMapping() throws FrontendException {
+        List<FuncSpec> funcList = new ArrayList<FuncSpec>();
+
+        funcList.add(new FuncSpec(this.getClass().getName(), schema(DataType.INTEGER)));
+        funcList.add(new FuncSpec(DoubleLog2.class.getName(), schema(DataType.DOUBLE)));
+        funcList.add(new FuncSpec(LongLog2.class.getName(), schema(DataType.LONG)));
+        return funcList;
+    }
+
+    private Schema schema(byte type) {
+        Schema s = new Schema();
+        s.add(new Schema.FieldSchema(null, type));
+        return s;
     }
 }
