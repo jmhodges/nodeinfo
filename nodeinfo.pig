71f6bd0a0aea6bbe80caf5ce79d750c8a2d30805
Jeff Hodges <jeff@somethingsimilar.com>
2009-12-07 00:18:59 -0800

    Parsing syslog files can now happen with nodeinfo.WordedSyslog. With this also comes a renaming of "rawword" to "word".

--- nodeinfo.pig
+++ nodeinfo.pig
@@ -1,27 +1,28 @@
 REGISTER nodeinfo.jar
 
-WORDS = LOAD 'parsedsyslog' AS (rawword: chararray, position: int, node: chararray, hour: int);
+LINE = LOAD 'smallsystem.log' USING TextLoader();
+WORD = FOREACH LINE GENERATE FLATTEN(nodeinfo.WordedSyslog($0));
 
-YGROUP = GROUP WORDS BY (rawword, position, node, hour);
-Y = FOREACH YGROUP GENERATE group, COUNT(WORDS) as wordpernodeperhourcount;
+YGROUP = GROUP WORD BY (word, position, node, hour);
+Y = FOREACH YGROUP GENERATE group, COUNT(WORD) as wordpernodeperhourcount;
 
-XGROUP = GROUP Y BY (group.rawword, group.position, group.node);
+XGROUP = GROUP Y BY (group.word, group.position, group.node);
 X = FOREACH XGROUP GENERATE group, SUM(Y.wordpernodeperhourcount) as wordpernodecount;
 
 XWITHLOG = FOREACH X GENERATE group, wordpernodecount, (wordpernodecount*nodeinfo.LOG2(wordpernodecount)) as weirdwordpernode;
 
-WORDGROUP = GROUP XWITHLOG BY (group.rawword, group.position);
+WORDGROUP = GROUP XWITHLOG BY (group.word, group.position);
 DANDE = FOREACH WORDGROUP GENERATE group, SUM(XWITHLOG.wordpernodecount) as dw, SUM(XWITHLOG.weirdwordpernode) as ew;
 
 ALLGW = FOREACH DANDE GENERATE group, (1.0 + (1.0 / nodeinfo.LOG2($numofnodes)) * (ew / dw - nodeinfo.LOG2(dw))) as gw;
 GW = FILTER ALLGW BY gw > 0.0;
 
-YANDG = JOIN GW BY (group.rawword, group.position), Y BY (group.rawword, group.position);
+YANDG = JOIN GW BY (group.word, group.position), Y BY (group.word, group.position);
 
-PARTIAL = FOREACH YANDG GENERATE Y::group.rawword as rawword, Y::group.position as position, Y::group.node as node, Y::group.hour as hour, (GW::gw * nodeinfo.LOG2(Y::wordpernodeperhourcount)) as partial;
+PARTIAL = FOREACH YANDG GENERATE Y::group.word as word, Y::group.position as position, Y::group.node as node, Y::group.hour as hour, (GW::gw * nodeinfo.LOG2(Y::wordpernodeperhourcount)) as partial;
 
 -- wtf how is one supposed to use piggybank's POW? arghhhhh.
-PARTIAL2 = FOREACH PARTIAL GENERATE rawword, position, node, hour, partial*partial as partial;
+PARTIAL2 = FOREACH PARTIAL GENERATE word, position, node, hour, partial*partial as partial;
 
 FULLGROUP = GROUP PARTIAL2 BY (node, hour);
 NODEINFO = FOREACH FULLGROUP GENERATE group.node, group.hour, SUM(PARTIAL2.partial) as oddness;
---
+++ src/main/java/WordedSyslog.java
@@ -0,0 +1,116 @@
+package nodeinfo;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import org.apache.pig.EvalFunc;
+import org.apache.pig.backend.executionengine.ExecException;
+import org.apache.pig.data.BagFactory;
+import org.apache.pig.data.DataBag;
+import org.apache.pig.data.DataType;
+import org.apache.pig.data.Tuple;
+import org.apache.pig.data.TupleFactory;
+import org.apache.pig.impl.logicalLayer.FrontendException;
+import org.apache.pig.impl.logicalLayer.schema.Schema;
+import org.apache.pig.FuncSpec;
+
+public class WordedSyslog extends EvalFunc<DataBag> {
+    BagFactory mBagFactory = BagFactory.getInstance();
+    TupleFactory mTupleFactory = TupleFactory.getInstance();
+    int returnTupleSize = 4;
+    
+    // returns a bag of tuples with word, position, node, and hour in them
+    // position being the position of the word in the line
+    public DataBag exec(Tuple input) throws IOException {
+        try {
+            if (input == null || input.size() == 0) return null;
+            
+            String line = (String) input.get(0);
+            if (line == null) return null;
+            
+            DataBag output = mBagFactory.newDefaultBag();
+            
+            addWordTuples(output, line);
+            return output;
+        } catch (ExecException ee) {
+            // wtf, pig. copy-pasta from TOKENIZE.java
+            throw ee;
+        }
+    }
+
+    @Override
+    public Schema outputSchema(Schema input) {
+        try {
+            Schema bagSchema = new Schema(tupleSchema());
+            bagSchema.setTwoLevelAccessRequired(true);
+            Schema.FieldSchema bagFS = new Schema.FieldSchema("bag_of_word_tuples",
+                                                              bagSchema,
+                                                              DataType.BAG
+                                                              );
+            return new Schema(bagFS);
+        } catch (FrontendException e) {
+            // stupidest fucking copy-pasta from TOKENIZE.java
+            // throwing RTE because
+            //above schema creation is not expected to throw an exception
+            // and also because superclass does not throw exception
+            throw new RuntimeException("Unable to compute WordedSyslog schema.");
+        }   
+    }
+    
+    public Schema.FieldSchema tupleSchema() throws FrontendException {
+        Schema s = new Schema();
+        s.add(new Schema.FieldSchema("word", DataType.CHARARRAY));
+        s.add(new Schema.FieldSchema("position", DataType.INTEGER));
+        s.add(new Schema.FieldSchema("node", DataType.CHARARRAY));
+        s.add(new Schema.FieldSchema("hour", DataType.INTEGER));
+        Schema.FieldSchema tupleFS = new Schema.FieldSchema("tuple_of_one_word_info",
+                                                            s,
+                                                            DataType.TUPLE
+                                                            );
+        return tupleFS;
+    }
+
+    private void addWordTuples(DataBag output, String line) throws ExecException {
+        StringTokenizer tok = tokenize(line);
+
+        tok.nextToken(); tok.nextToken(); // throw away month and day
+        Integer hour = Integer.valueOf(tok.nextToken());
+
+        tok.nextToken(); tok.nextToken(); // throw away minute and second
+        String node = tok.nextToken();
+
+        // throw away the first "word" as it's just the process name.
+        // Including it has been found to make the algo useless, so we
+        // just don't.
+        tok.nextToken();
+
+        int pos = 0;
+        while (tok.hasMoreTokens()) {
+            Tuple tup = mTupleFactory.newTuple(returnTupleSize);
+            tup.set(0, tok.nextToken());
+            tup.set(1, pos);
+            tup.set(2, node);
+            tup.set(3, hour);
+            output.add(tup);
+            pos++;
+        }
+    }
+
+    private StringTokenizer tokenize(String line) {
+        StringTokenizer tok =  new StringTokenizer(line, " \",()*:", false);
+        if (tok.countTokens() < 7) {
+            throw new RuntimeException("Malformed syslog line: "+line);
+        }
+        return tok;
+    }
+
+    public List<FuncSpec> getArgToFuncMapping() throws FrontendException {
+        List<FuncSpec> funcList = new ArrayList<FuncSpec>();
+        Schema s = new Schema();
+        s.add(new Schema.FieldSchema(null, DataType.CHARARRAY));
+        funcList.add(new FuncSpec(this.getClass().getName(), s));
+        return funcList;
+    }
+}
